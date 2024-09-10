f2dbd0666055fe2ae7ed48ca8f38242cec230f7f
Thiago Contardi <thiagocontardi@hotmail.com>
2024-09-10 11:24:17 -0300

    fix: add fingerprint method in define

--- view/frontend/web/js/fingerprint.js
+++ view/frontend/web/js/fingerprint.js
@@ -1,4 +1,4 @@
-define([], function () {
+define(['uiComponent'], function (Component) {
     'use strict';
     return function (sandbox) {
         if (typeof window.yapayFingerprintLoaded == 'undefined') {
