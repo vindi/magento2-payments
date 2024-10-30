d0813807690957cc61444e8f9981eb3e7f572b06
Iago Cedran <iago@bizcommerce.com.br>
2024-10-30 16:42:07 +0000

    refactor: adding validation on payment methods and payment link success page

--- Controller/Checkout/Success.php
+++ Controller/Checkout/Success.php
@@ -21,6 +21,7 @@ use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Message\ManagerInterface;
 use Vindi\VP\Helper\Data;
 use Vindi\VP\Model\PaymentLinkService;
 
@@ -52,25 +53,32 @@ class Success implements HttpGetActionInterface
     private Data $helperData;
 
     /**
+     * @var ManagerInterface
+     */
+    private ManagerInterface $messageManager;
+
+    /**
      * @param PageFactory $resultPageFactory
      * @param PaymentLinkService $paymentLinkService
      * @param RequestInterface $request
      * @param RedirectFactory $redirectFactory
      * @param Data $helperData
+     * @param ManagerInterface $messageManager
      */
     public function __construct(
         PageFactory $resultPageFactory,
         PaymentLinkService $paymentLinkService,
         RequestInterface $request,
         RedirectFactory $redirectFactory,
-        Data $helperData
-    )
-    {
+        Data $helperData,
+        ManagerInterface $messageManager
+    ) {
         $this->resultPageFactory = $resultPageFactory;
         $this->paymentLinkService = $paymentLinkService;
         $this->request = $request;
         $this->redirectFactory = $redirectFactory;
         $this->helperData = $helperData;
+        $this->messageManager = $messageManager;
     }
 
     /**
@@ -81,16 +89,37 @@ class Success implements HttpGetActionInterface
         $result = $this->resultPageFactory->create();
         $orderId = $this->request->getParam('order_id');
         $order = $this->paymentLinkService->getOrderByOrderId($orderId);
+
+        if (!$order) {
+            $this->messageManager->addWarningMessage(__('Order does not exist.'));
+            return $this->redirectFactory->create()->setPath('/');
+        }
+
         $orderStatus = $order->getStatus();
         $configStatus = $this->helperData->getConfig('order_status', $order->getPayment()->getMethod());
         $isCcMethod = str_contains($order->getPayment()->getMethod(), 'cc');
 
+        if ($order->hasInvoices()) {
+            $this->messageManager->addWarningMessage(__('Order already has an invoice.'));
+            return $this->redirectFactory->create()->setPath('/');
+        }
+
+        $paymentLinkStatus = $this->paymentLinkService->getPaymentLinkStatus($orderId);
+        if ($paymentLinkStatus === 'expired') {
+            $this->messageManager->addWarningMessage(__('This payment link has expired.'));
+            return $this->redirectFactory->create()->setPath('/');
+        }
+
         try {
             if (!$orderId || (!$isCcMethod && $orderStatus !== $configStatus)) {
-                return $this->redirectFactory->create()->setPath('noroute');
+                return $this->redirectFactory->create()->setPath('/');
             }
         } catch (\Exception $e) {
-            return $this->redirectFactory->create()->setPath('noroute');
+            $this->messageManager->addErrorMessage(
+                __('An error occurred while processing your request. Please try again later.')
+            );
+
+            return $this->redirectFactory->create()->setPath('/');
         }
 
         return $result;
