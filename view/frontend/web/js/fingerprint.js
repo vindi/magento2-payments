56883521f94872d986bf2e7979de9aa4aee887ef
Thiago Contardi <thiagocontardi@hotmail.com>
2024-10-30 12:03:47 -0300

    fix: remove await vindi fingerprint

--- view/frontend/web/js/fingerprint.js
+++ view/frontend/web/js/fingerprint.js
@@ -2,22 +2,15 @@ define(['uiComponent'], function (Component) {
     'use strict';
     return function (sandbox) {
         if (typeof window.yapayFingerprintLoaded == 'undefined') {
-            function loadScript() {
-                return new Promise((resolve) => {
-                    let fpOptions = {env: 'production'};
-                    if (parseInt(sandbox) === 1) {
-                        fpOptions.env = 'sandbox';
-                    }
-                    window?.yapay?.FingerPrint(fpOptions);
-                    //Wait 1 sec because it was blocking checkout page
-                    setTimeout(() => {
-                        resolve();
-                    }, 1000);
-                });
-            }
-            loadScript().then(() => {
+            async function loadScript() {
+                let fpOptions = {env: 'production'};
+                if (parseInt(sandbox) === 1) {
+                    fpOptions.env = 'sandbox';
+                }
+                await window?.yapay?.FingerPrint(fpOptions);
                 window.yapayFingerprintLoaded = true;
-            });
+            }
+            loadScript();
         }
     };
 });
