815ce7600bb00520b126543279a5e6cc3dccd85d
Iago Cedran <iago@bizcommerce.com.br>
2025-01-28 14:17:13 +0000

    feat: save cart_token

--- Gateway/Response/CreditCard/TransactionHandler.php
+++ Gateway/Response/CreditCard/TransactionHandler.php
@@ -28,6 +28,8 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Payment\Gateway\Response\HandlerInterface;
+use Vindi\VP\Model\CreditCardFactory;
+use Magento\Framework\Exception\AlreadyExistsException;
 
 class TransactionHandler implements HandlerInterface
 {
@@ -43,16 +45,21 @@ class TransactionHandler implements HandlerInterface
     /** @var Api */
     protected $api;
 
+    /** @var CreditCardFactory */
+    protected $creditCardFactory;
+
     public function __construct(
         HelperOrder $helperOrder,
         Data $helper,
         SessionManagerInterface $session,
-        Api $api
+        Api $api,
+        CreditCardFactory $creditCardFactory
     ) {
         $this->helperOrder = $helperOrder;
         $this->helper = $helper;
         $this->session = $session;
         $this->api = $api;
+        $this->creditCardFactory = $creditCardFactory;
     }
 
     /**
@@ -87,6 +94,8 @@ class TransactionHandler implements HandlerInterface
         $responseTransaction = $transaction['data_response']['transaction'];
         $payment = $this->helperOrder->updateDefaultAdditionalInfo($payment, $responseTransaction);
 
+        $this->saveCreditCardData($payment, $responseTransaction);
+
         if (
             $responseTransaction['status_id'] == HelperOrder::STATUS_PENDING
             || $responseTransaction['status_id'] == HelperOrder::STATUS_MONITORING
@@ -97,4 +106,33 @@ class TransactionHandler implements HandlerInterface
         }
     }
 
+    /**
+     * Save credit card data to the database
+     *
+     * @param \Magento\Sales\Model\Order\Payment $payment
+     * @param array $responseTransaction
+     * @throws AlreadyExistsException
+     */
+    protected function saveCreditCardData($payment, array $responseTransaction)
+    {
+        $cardData = $responseTransaction['payment'];
+
+        if (isset($cardData['card_token']) && isset($cardData['payment_method_name'])) {
+            /** @var \Vindi\VP\Model\CreditCard $creditCard */
+            $creditCard = $this->creditCardFactory->create();
+            $creditCard->setData([
+                'customer_id' => $payment->getOrder()->getCustomerId(),
+                'card_token' => $cardData['card_token'],
+                'customer_email' => $payment->getOrder()->getCustomerEmail(),
+                'status' => $responseTransaction['status_name'],
+                'type' => $cardData['payment_method_name'],
+                'cc_type' => $responseTransaction['status_name'],
+                'cc_last_4' => '',
+                'cc_name' => $payment->getOrder()->getBillingAddress()->getFirstname(),
+                'cc_exp_date' => '',
+                'cc_number' => '',
+            ]);
+            $creditCard->save();
+        }
+    }
 }
